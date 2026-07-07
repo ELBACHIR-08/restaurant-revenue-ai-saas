f62de7f95e2ad005e754f079654a47089cd8ceeb
ELBACHIR-08 <mouhamadouelbachir@gmail.com>
2026-07-07 10:42:23 +0000

    chore: restructure repository for Vercel deployment

--- api/me.js
+++ api/me.js
@@ -1,5 +1,5 @@
-const { send, handleOptions, getBearer } = require('../_lib/http');
-const { getUserFromToken, getMemberships, isPlatformAdmin } = require('../_lib/supabase-rest');
+const { send, handleOptions, getBearer } = require('./_lib/http');
+const { getUserFromToken, getMemberships, isPlatformAdmin } = require('./_lib/supabase-rest');
 
 module.exports = async (req, res) => {
   if (handleOptions(req, res)) return;
